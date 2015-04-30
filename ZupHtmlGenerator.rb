52e41ec6463de6cb5a499ba6ea4210c39e0e9fe0
Roberto Antelo <roberto_antelo@hotmail.com>
2015-04-29 22:42:53 -0500

    Update ZupHtmlGenerator.rb

--- ZupHtmlGenerator.rb
+++ ZupHtmlGenerator.rb
@@ -1,10 +1,10 @@
-require_relative 'Message.rb'
-require_relative 'HtmlStringsUtilities.rb'
+require_relative 'message.rb'
+require_relative 'html_strings_utilities.rb'
 
 file = ARGV[0].to_s
 user = ARGV[1].to_s
 
-htmlUtil = HtmlStringsUtilities.new(user)
+html_util = html_strings_utilities.new(user)
 new_file_regex = {/ / => '_', /.txt/ => '.html'}
 new_file = ""
 new_file_regex.each do |key, value|
@@ -12,17 +12,17 @@ new_file_regex.each do |key, value|
 end
 
 File.open(new_file, "a+") do |html|
-	html.puts htmlUtil.start
+	html.puts html_util.start
 	prev_message = Message.new("")
 	File.open(file, 'r').each_line do |line|
 		message = Message.new(line)
 		if message.sender == ""
 			prev_message.msg += "#{message.msg}"
 		else
-			html.puts htmlUtil.bubble(prev_message)
+			html.puts html_util.bubble(prev_message)
 			prev_message = message
 		end
 	end
-	html.puts htmlUtil.bubble(prev_message)
-	html.puts htmlUtil.end
+	html.puts html_util.bubble(prev_message)
+	html.puts html_util.end
 end	
